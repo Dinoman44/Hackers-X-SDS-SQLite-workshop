8adc74f70e7c36cb5d0da630ee1c0ef27c36737e
Dinoman44 <prakamya.singh08@gmail.com>
2025-03-15 19:45:05 +0800

    Rename and reorganize example query files

--- examples/filtering_aggregations.sql
+++
@@ -1,3 +0,0 @@
--- SELECT country, COUNT(id) FROM customer WHERE COUNT(id) > 5 GROUP BY country;
-
-SELECT country, COUNT(id) FROM customer GROUP BY country HAVING COUNT(id) > 5;
--- examples/older_than_30.sql
+++
@@ -1,1 +0,0 @@
-SELECT name, age FROM customer WHERE age > 30;
